e1923092851307ebfba7042b5b8bf98ba8b5a072
Vas9lisa <vas9lisa.tort@gmail.com>
2022-06-28 22:11:14 +0300

    dz

--- JS_2yrok.js
+++ JS_2yrok.js
@@ -13,3 +13,14 @@ console.log(true && false || true)
 console.log(true && false || true && false)
 console.log(true && false || true && true)
 // И (&&) будет как умножение, а ИЛИ (||) будет как сложение - при очередности действия в примерах.
+
+if(true && true || false){
+    console.log("1")
+} else {
+    console.log("2")
+}
+
+let age = 18
+let money = 1000
+let dog =true
+
